e0d75471a7bd1aedda824b45e6ae6892cd705325
Aviral Verma <aviralverma.verma@gmail.com>
2019-10-30 13:46:38 +0530

    Create URLify a given string.cpp

---
+++ GeeksforGeeks/URLify a given string.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+ {
+	int t;
+	cin >> t;
+	while(t>0)
+	{
+	    string s;
+	    cin.ignore();
+	    getline(cin, s);
+	    int size;
+	    cin >> size;
+	    stringstream check(s);
+	    string token;
+	    vector<string> tokens;
+	    while(getline(check, token, ' '))
+	    {
+	        tokens.push_back(token);
+	    }
+	    int tokensize = tokens.size();
+	    string result = tokens[0];
+	    int i = 1;
+	    while(i<tokensize)
+	    {
+	        result=result+"%20"+tokens[i];
+	        i++;
+	    }
+	    cout << result <<endl;
+	    
+	    
+	    t--;
+	}
+	return 0;
+}
