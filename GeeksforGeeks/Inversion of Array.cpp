b8475d61b41eb697739636693dfd4691b70573b5
Aviral Verma <aviralverma.verma@gmail.com>
2019-10-31 00:38:45 +0530

    Update Inversion of Array.cpp

--- GeeksforGeeks/Inversion of Array.cpp
+++ GeeksforGeeks/Inversion of Array.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+
 void merge(int a[], int l, int m, int r, long long int &inversionCount){
     int arr1[m-l+1];
     int arr2[r-m];
@@ -24,16 +25,12 @@ void merge(int a[], int l, int m, int r, long long int &inversionCount){
         {
             a[k++]=arr2[j++];
             inversionCount+=(m-l+1-i);
-        }
-        
+        }    
     }   
     while(i<(m-l+1))
-    a[k++]=arr1[i++];
-        
+        a[k++]=arr1[i++];   
     while(j<(r-m))
-    a[k++]=arr2[j++]; 
-        
-    
+        a[k++]=arr2[j++];    
 }
 void mergeSort(int a[], int l, int r, long long int &inversionCount)
 {
@@ -65,5 +62,4 @@ int main() {
         t--;
     }
         return 0;
-
 }
