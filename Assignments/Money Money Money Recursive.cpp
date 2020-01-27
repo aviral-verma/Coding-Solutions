489148e13b12ec563d719e8445dd455d02f83b8b
Aviral Verma <aviralverma.verma@gmail.com>
2020-01-28 00:03:14 +0530

    Update Money Money Money Recursive.cpp

--- Assignments/Money Money Money Recursive.cpp
+++ Assignments/Money Money Money Recursive.cpp
@@ -5,18 +5,38 @@
 #include <algorithm>
 using namespace std;
 
-int transaction(int n, int m) {
+long long int power(int a, int b)
+{
+    long long int i;
+    long long int ans = 1;
+    for(i=0; i<b; i++)
+    {
+        ans = (ans*a)%1000000007;
+    }
+    return ans;
+}
+unsigned long long int transaction(int n, int m) {
+    if(m==0)
+        return 1;
     if(n==1||m==1)
         return 0;
     else 
-        return pow((n-1),(m-1)) - transaction(n, m-1);
+    {
+        if(power((n-1),(m-1))<transaction(n, m-1))
+           {
+            return power((n-1),(m-1)) + 1000000007 - transaction(n, m-1);
+           }
+        else
+            return power((n-1),(m-1)) - transaction(n, m-1);
+    }
+
 }
 int main() {
     int n;
     int m;
     cin >> n;
     cin >> m;
-    int result = transaction(n,m);
+    long long int result = transaction(n,m);
     cout << result;
     return 0;
 }
