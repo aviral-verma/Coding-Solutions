d45b9cbf61af64f887eebe659ddf1cd261d64ecd
Aviral Verma <aviralverma.verma@gmail.com>
2020-01-29 19:21:02 +0530

    Update Money Money Money Iterative.cpp

--- Assignments/Money Money Money Iterative.cpp
+++ Assignments/Money Money Money Iterative.cpp
@@ -5,30 +5,43 @@
 #include <algorithm>
 using namespace std;
 
-long long int power(int a, int b) {
-    long long int i;
-    long long int ans = 1;
-    for(i=0; i<b; i++)
+long long int power(long long int a, long long int b) {
+    if(b==0)
+        return 1;
+    else if(b%2==0)
     {
-        ans = (ans*a)%1000000007;
+        return (power(a, b/2)*power(a, b/2))%1000000007;
     }
-    return ans;
+    else return ((a*power(a, b/2))%1000000007*power(a, b/2))%1000000007;
 }
 
-long long int transaction(int n, int m) {
+long long int transaction(long long int n, long long int m) {
+    long long int i, term;
     if(m==0)
         return 1;
     if(n==1||m==1)
         return 0;
-    else 
+    if(m==2)
+        return n-1;
+    else
     {
-        
+        term = n-1;
+        for(i=3; i<=m; i++)
+        {
+            if(power((n-1), (i-1)) < term)
+            {
+                term = power((n-1), (i-1)) + 1000000007 - term;
+            }
+            else
+            term = power((n-1), (i-1)) - term;
+        }
     }
+    return term;
 }
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    int n;
-    int m;
+    long long int n;
+    long long int m;
     cin >> n;
     cin >> m;
     long long int result = transaction(n,m);
